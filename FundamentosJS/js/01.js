171fb7a4512f2dd0b4fc446fcb2b5543497e8a4c
Claudia Villarroel <claudia.villarroel@live.com>
2023-09-03 17:11:41 -0300

    Clase: Variables con let

---
+++ FundamentosJS/js/01.js
@@ -0,0 +1,11 @@
+// Variables : let y conts
+
+let cliente = "Juan";
+let precioDescuento = 220; //camelCase
+
+console.log(cliente);
+console.log(precioDescuento);
+
+let precio;
+precio = 100
+console.log(precio)
