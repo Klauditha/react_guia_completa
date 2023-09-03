13b0e4fe17c815c6568d48b1ce12e3e5a1551d91
Claudia Villarroel <claudia.villarroel@live.com>
2023-09-03 17:18:32 -0300

    Variables con const

---
+++ FundamentosJS/js/02.js
@@ -0,0 +1,11 @@
+const cliente = "Juan";
+const precioDescuento = 220; //camelCase
+
+//No se puede reasignar
+cliente = 20
+console.log(cliente);
+console.log(precioDescuento);
+
+
+//No se puede iniciar sin valor
+const a;
